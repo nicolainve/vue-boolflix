0512c6f8b331bbd36d31a129866c9e5b1fc36f69
Nicola <nginvernizzi1@gmail.com>
2020-11-25 16:30:17 +0100

    milestone 1 done

--- js/main.js
+++ js/main.js
@@ -2,23 +2,37 @@ const app = new Vue({
     el: '#app',
     data: {
         inputText: '',
-        films: []
+        films: [],
+        tvs: []
     },
     methods: {
         searchItem() {
+            if (this.inputText.length > 0) {
+                axios.get('https://api.themoviedb.org/3/search/movie', {
+                    params: {
+                        api_key: '7ce64d05ee459f4b7de455e40f7bea40',
+                        query: this.inputText,
+                    }
+                })
+                .then(response => {
+                    this.films = response.data.results
+                })
+                .catch(error => {console.log(error)});
 
-
-            axios.get('https://api.themoviedb.org/3/search/movie', {
-                params: {
-                    api_key: '7ce64d05ee459f4b7de455e40f7bea40',
-                    query: this.inputText,
-                    language: 'it'
-                }
-            })
-            .then(response => {
-                this.films = response.data.results
-            })
-            .catch(error => {console.log(error)});
+                axios.get('https://api.themoviedb.org/3/search/tv', {
+                    params: {
+                        api_key: '7ce64d05ee459f4b7de455e40f7bea40',
+                        query: this.inputText,
+                    }
+                })
+                .then(response => {
+                    this.tvs = response.data.results
+                })
+                .catch(error => {console.log(error)});
+            }
+        },
+        getStars(vote) {
+            return Math.ceil(vote / 2)
         }
     }
-})
+});
