ba62fc8349ac7975ebd2e12bc594eee7ac46333f
Nicola <nginvernizzi1@gmail.com>
2020-11-25 17:04:41 +0100

    added ISO Language Codes

--- js/main.js
+++ js/main.js
@@ -3,7 +3,17 @@ const app = new Vue({
     data: {
         inputText: '',
         films: [],
-        tvs: []
+        tvs: [],
+        language: '',
+        languages: []
+    },
+    created() {
+        axios.get('https://pkgstore.datahub.io/core/language-codes/language-codes_json/data/97607046542b532c395cf83df5185246/language-codes_json.json')
+        .then(response => {
+            this.languages = response.data
+            console.log(this.languages);
+        })
+        .catch(error => { console.log(error)})
     },
     methods: {
         searchItem() {
@@ -12,6 +22,7 @@ const app = new Vue({
                     params: {
                         api_key: '7ce64d05ee459f4b7de455e40f7bea40',
                         query: this.inputText,
+                        language: this.language
                     }
                 })
                 .then(response => {
@@ -23,6 +34,7 @@ const app = new Vue({
                     params: {
                         api_key: '7ce64d05ee459f4b7de455e40f7bea40',
                         query: this.inputText,
+                        language: this.language
                     }
                 })
                 .then(response => {
